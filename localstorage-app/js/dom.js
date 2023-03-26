f5d70ef3b8b68c159075e7daeaff64eb1ffbd990
= <bdrgcmz@gmail.com>
2023-03-26 22:54:52 +0200

    Updated to do project

--- localstorage-app/js/dom.js
+++
@@ -1,4 +0,0 @@
-/* Courses will be rendered to this container */
-const courseContainer = document.getElementById("course-container");
-/* New course add form. For the Submit event */
-const courseForm = document.getElementById("course-form");
--- to_do_list/app.js
+++ to_do_list/app.js
@@ -6,43 +6,72 @@ const itemAddText = document.querySelector("#list-item-input");
 const itemAddButton = document.getElementById("list-item-add-button");
 const deleteAll = document.querySelector("#delete-all");
 let taskArray = [];
+
 /**
  * Array render to do list "First task","Second task"
  */
-function renderToDoList(){
-    cardBody.innerHTML = taskArray.map((item,index) => {
-    return `
+function renderToDoList() {
+  cardBody.innerHTML = taskArray
+    .map((item, index) => {
+      return `
         <div class="list-item">
-            <li class="list-item-text">${item}</li>
-            <i onclick="removeItem(${index})" class="far fa-trash-alt"></i>
+            <li id="${index}" class="list-item-text ${item.done_task_text}">${item.text}</li>
+            <div> 
+                <i onclick="removeItem(${index})" class="far fa-trash-alt"></i>
+                <i id="${index}icon" onclick="taskDone(${index})"  class="fa-solid fa-circle-check ${item.done_task_icon}"></i>
+            </div>
         </div>
-    `
-    }).join("");
-};
+    `;
+    })
+    .join("");
+}
 /**
- * Add new ask 
+ * Add new ask
  */
-itemAddButton.addEventListener("click", function (){
-    let inputValue = itemAddText.value;
-    if(inputValue == ""){
-        alert("This not a task!!")
-    }else{
-        taskArray.push(inputValue)
-    }
-    itemAddText.value = ""
+
+itemAddButton.addEventListener("click", function () {
+  let inputValue = itemAddText.value;
+  if (inputValue == "") {
+    alert("This not a task!!");
+  } else {
+    const listItem = {
+      text: inputValue,
+      done_task_text: "",
+      done_task_icon: "",
+    };
+    taskArray.push(listItem);
+  }
+  itemAddText.value = "";
+  renderToDoList();
+});
+
+/**
+ * Task done event
+ */
+const taskDone = (pIndex) => {
+  const chooseTask = taskArray.find((item, index) => index === pIndex);
+  if (chooseTask.done_task_text === "") {
+    chooseTask.done_task_text = "task-done";
+    chooseTask.done_task_icon = "task-done-icon";
     renderToDoList();
-})
+  } else {
+    chooseTask.done_task_text = "";
+    chooseTask.done_task_icon = "";
+    renderToDoList();
+  }
+};
+
 /**
  * Remove an item
  */
-function removeItem(pItem){
-    taskArray.splice(pItem,1)
-    renderToDoList();
+function removeItem(pItem) {
+  taskArray.splice(pItem, 1);
+  renderToDoList();
 }
 /**
  * Remove all item
  */
-deleteAll.addEventListener("click", function(){
-    cardBody.innerHTML = "";
-    taskArray = [];
+deleteAll.addEventListener("click", function () {
+  cardBody.innerHTML = "";
+  taskArray = [];
 });
