ab0eb8e38ed291140046d3e8e3161f2c49a542ea
= <bdrgcmz@gmail.com>
2023-03-27 10:48:34 +0200

    Added localstorage

--- to_do_list/app.js
+++ to_do_list/app.js
@@ -10,20 +10,25 @@ let taskArray = [];
 /**
  * Array render to do list "First task","Second task"
  */
-function renderToDoList() {
-  cardBody.innerHTML = taskArray
-    .map((item, index) => {
-      return `
-        <div class="list-item">
-            <li id="${index}" class="list-item-text ${item.done_task_text}">${item.text}</li>
-            <div> 
-                <i onclick="removeItem(${index})" class="far fa-trash-alt"></i>
-                <i id="${index}icon" onclick="taskDone(${index})"  class="fa-solid fa-circle-check ${item.done_task_icon}"></i>
+function renderToDoList(pArray) {
+  console.log(pArray[0]);
+  if (pArray[0] === undefined) {
+    cardBody.innerHTML = `<p class="not-todo">Have some fun today. You have nothing to do.<i class="fa-solid fa-hand-peace ms-2"></p>`;
+  } else {
+    cardBody.innerHTML = pArray
+      .map((item, index) => {
+        return `
+            <div class="list-item">
+                <li id="${index}" class="list-item-text ${item.done_task_text}">${item.text}</li>
+                <div> 
+                    <i onclick="removeItem(${index})" class="far fa-trash-alt"></i>
+                    <i id="${index}icon" onclick="taskDone(${index})"  class="fa-solid fa-circle-check ${item.done_task_icon}"></i>
+                </div>
             </div>
-        </div>
-    `;
-    })
-    .join("");
+        `;
+      })
+      .join("");
+  }
 }
 /**
  * Add new ask
@@ -39,25 +44,33 @@ itemAddButton.addEventListener("click", function () {
       done_task_text: "",
       done_task_icon: "",
     };
-    taskArray.push(listItem);
+    const taskArrayInLocal = getLocalStorage("todoList");
+    taskArrayInLocal.push(listItem);
+    setLocalStorage("todoList", taskArrayInLocal);
+    const newTaskArray = getLocalStorage("todoList");
+    renderToDoList(newTaskArray);
   }
   itemAddText.value = "";
-  renderToDoList();
 });
 
 /**
  * Task done event
  */
 const taskDone = (pIndex) => {
-  const chooseTask = taskArray.find((item, index) => index === pIndex);
+  const taskArrayInLocal = getLocalStorage("todoList");
+  const chooseTask = taskArrayInLocal.find((item, index) => index === pIndex);
   if (chooseTask.done_task_text === "") {
     chooseTask.done_task_text = "task-done";
     chooseTask.done_task_icon = "task-done-icon";
-    renderToDoList();
+    setLocalStorage("todoList", taskArrayInLocal);
+    const newTaskArray = getLocalStorage("todoList");
+    renderToDoList(newTaskArray);
   } else {
     chooseTask.done_task_text = "";
     chooseTask.done_task_icon = "";
-    renderToDoList();
+    setLocalStorage("todoList", taskArrayInLocal);
+    const newTaskArray = getLocalStorage("todoList");
+    renderToDoList(newTaskArray);
   }
 };
 
@@ -65,13 +78,34 @@ const taskDone = (pIndex) => {
  * Remove an item
  */
 function removeItem(pItem) {
-  taskArray.splice(pItem, 1);
-  renderToDoList();
+  const taskArrayInLocal = getLocalStorage("todoList");
+  taskArrayInLocal.splice(pItem, 1);
+  setLocalStorage("todoList", taskArrayInLocal);
+  const newTaskArray = getLocalStorage("todoList");
+  renderToDoList(newTaskArray);
 }
 /**
  * Remove all item
  */
 deleteAll.addEventListener("click", function () {
-  cardBody.innerHTML = "";
+  cardBody.innerHTML = `<p class="not-todo">Have some fun today. You have nothing to do.<i class="fa-solid fa-hand-peace ms-2"></i></p>`;
   taskArray = [];
+  setLocalStorage("todoList", taskArray);
 });
+/**
+ * LocalStorage add and undo player list functions
+ */
+const setLocalStorage = (pStringKey, pArrar) => {
+  localStorage.setItem(pStringKey, JSON.stringify(pArrar));
+};
+const getLocalStorage = (pStringKey) => {
+  return JSON.parse(localStorage.getItem(pStringKey));
+};
+
+/*If there is no array in localstorage, it will be thrown there first,
+It will be rendered later. If there is an array, it will be rendered directly.*/
+if (localStorage.getItem("todoList") === null) {
+  setLocalStorage("todoList", taskArray);
+}
+
+renderToDoList(getLocalStorage("todoList"));
